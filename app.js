dff509f43e676d0d1c8e4233db2ad84c121cb25c
Sizonenko Timofey <149585004+Nyarhotep@users.noreply.github.com>
2025-02-14 18:54:39 +0900

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const mainText = document.getElementById("mainText");
     const buttons = document.getElementById("buttons");
 
-    // Анимация появления картинки (скольжение из правой части)
-    setTimeout(() => {
-        mainImage.classList.remove("hidden");
-        mainImage.classList.add("visible");
-        mainImage.style.transform = "translateX(0)"; // Скольжение в центр
-    }, 500);
-
     // Анимация появления текста
     setTimeout(() => {
         mainText.classList.remove("hidden");
@@ -26,18 +19,18 @@ document.addEventListener("DOMContentLoaded", function () {
     // Обработка нажатия на кнопку "Да"
     document.getElementById("yesButton").addEventListener("click", function () {
         // Затухание первой картинки
-        mainImage.style.opacity = "0";
+        mainImage.style.animation = "fadeOut 1s forwards";
 
         // Появление второй картинки
         setTimeout(() => {
-            secondImage.style.opacity = "1";
-        }, 500); // Задержка для плавного перехода
+            secondImage.style.animation = "fadeIn 1s forwards";
+        }, 1000); // Задержка для плавного перехода
 
         // Поднимаем текст вверх
         mainText.style.transform = "translateY(-50px)";
 
         // Исчезновение кнопок
-        buttons.style.opacity = "0";
+        buttons.style.animation = "fadeOut 1s forwards";
         setTimeout(() => {
             buttons.style.display = "none"; // Убираем кнопки из DOM
         }, 1000); // Ждем завершения анимации
